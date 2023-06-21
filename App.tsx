4b1e2f51a9f4af9063915ad6dac878a57a67f65f
Lucas Souza <lucasnando800@hotmail.com>
2023-06-20 23:18:09 -0300

    SignIn & SignUp Page

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { View, StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+
+import { Routes } from "@routes/index";
+import { Loading } from "@components/Loading";
+
+import { THEME } from "./src/theme";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+
+      {fontsLoaded ? <Routes /> : <Loading />}
+    </NativeBaseProvider>
+  );
+}
---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import { Button as NativeBaseButton, IButtonProps, Text } from "native-base";
+
+type Props = IButtonProps & {
+  title: string;
+  variant?: 'solid' | 'outline'
+};
+
+export function Button({ title, variant = "solid", ...rest }: Props) {
+  return (
+    <NativeBaseButton
+      w="full"
+      h={14}
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor="green.500"
+      rounded="sm"
+      _pressed={{
+        bg: variant === "outline" ? "gray.500" : "green.500",
+      }}
+      {...rest}
+    >
+      <Text color={variant === "outline" ? "green.500" : "white"} fontFamily="heading" fontSize="sm">
+        {title}
+      </Text>
+    </NativeBaseButton>
+  );
+}
