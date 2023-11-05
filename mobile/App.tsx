fe97da1bda29be5da7f748d3d1f0d60fed163f21
Lucas Souza <lucasnando800@hotmail.com>
2023-11-05 18:30:05 -0300

    Add OneSignal

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,5 @@
 import { View, StatusBar } from "react-native";
+import { OneSignal } from "react-native-onesignal";
 import { NativeBaseProvider } from "native-base";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 
@@ -8,6 +9,8 @@ import { Loading } from "@components/Loading";
 import { AuthContextProvider } from "@contexts/AuthContext";
 
 import { THEME } from "./src/theme";
+
+OneSignal.initialize("31d29d14-61df-4b1f-b849-d5a75c72e1b7");
 
 export default function App() {
   const [fontsLoaded] = useFonts({
